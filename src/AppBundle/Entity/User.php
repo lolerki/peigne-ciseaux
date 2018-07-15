1df649bd7311b11d37a7b65f832b18c7cf4c4165
lolerki <virgilmeaup@hotmail.fr>
2018-07-15 15:56:31 +0200

    delete fileds name in entity user

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -20,20 +20,6 @@ class User extends BaseUser {
     protected $id;
 
     /**
-     * @ORM\Column(type="string", length=255)
-     *
-     * @Assert\NotBlank(message="Please enter your name.", groups={"Registration", "Profile"})
-     * @Assert\Length(
-     *     min=3,
-     *     max=255,
-     *     minMessage="The name is too short.",
-     *     maxMessage="The name is too long.",
-     *     groups={"Registration", "Profile"}
-     * )
-     */
-    protected $name;
-
-    /**
      * @var string
      *
      * @ORM\Column(name="last_name", type="string", length=50, nullable=true)
@@ -314,27 +300,4 @@ class User extends BaseUser {
         return $this->zipCode;
     }
 
-    /**
-     * Set name
-     *
-     * @param string $name
-     *
-     * @return User
-     */
-    public function setName($name)
-    {
-        $this->name = $name;
-
-        return $this;
-    }
-
-    /**
-     * Get name
-     *
-     * @return string
-     */
-    public function getName()
-    {
-        return $this->name;
-    }
 }
